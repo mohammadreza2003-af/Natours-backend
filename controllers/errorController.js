cbd312ecb584de3de8837fb7c46ed374f3d24cec
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-05-12 11:22:17 +0430

    fix ignore

---
+++ controllers/errorController.js
@@ -0,0 +1,60 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  console.log(err);
+  const value = err.errorResponse.errmsg.match(/(["'])(.*?)\1/)[0];
+  const message = `Duplicate field value: ${value}. please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationError = err => {
+  const errorFields = Object.values(err.errors).map(e => e.message);
+  const message = `Invalid input data. ${errorFields.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    message: err.message,
+    status: err.status,
+    stack: err.stack,
+    error: err
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      message: err.message,
+      status: err.status
+    });
+  } else {
+    console.error('Error', err);
+
+    res.status(500).json({
+      message: 'Something went very wrong!',
+      status: 'fail'
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationError(error);
+
+    sendErrorProd(error, res);
+  }
+};
